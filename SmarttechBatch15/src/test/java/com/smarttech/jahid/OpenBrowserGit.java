a806250ac26ab23da66d50255ae156654ec58fa9
mislam <mislam@microdesk.com>
2021-07-03 11:37:35 -0400

    open browse code

---
+++ SmarttechBatch15/src/test/java/com/smarttech/jahid/OpenBrowserGit.java
@@ -0,0 +1,11 @@
+package com.smarttech.jahid;
+
+public class OpenBrowserGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Git");
+
+	}
+
+}
